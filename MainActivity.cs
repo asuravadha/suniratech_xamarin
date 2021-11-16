1ce336bb8e99ca23156298471322927d47cc264d
asuravadha <rr.easwar@gmail.com>
2021-11-16 10:04:10 +0530

    Add project files.

---
+++ MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using Android.Views;
+
+namespace suniratech_xamarin
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            
+            EditText username = FindViewById<EditText>(Resource.Id.username);
+            EditText password = FindViewById<EditText>(Resource.Id.password);
+            EditText email = FindViewById<EditText>(Resource.Id.email);
+            Button submit = FindViewById<Button>(Resource.Id.submit);
+            TextView result=FindViewById<TextView>(Resource.Id.result);
+            submit.Click += delegate{ result.Text = "username :"+username.Text+"\npassword :"+password.Text+"\nemail :"+email.Text; };
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
