78a4246327a8f02d56c61c1a02a4d7a2e124f4ec
asuravadha <rr.easwar@gmail.com>
2021-11-16 11:23:55 +0530

    nov 16

--- MainActivity.cs
+++ MainActivity.cs
@@ -10,6 +10,11 @@ namespace suniratech_xamarin
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        EditText username;
+        EditText password;
+        EditText email;
+        Button submit;
+        TextView result;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -18,11 +23,11 @@ namespace suniratech_xamarin
             SetContentView(Resource.Layout.activity_main);
 
             
-            EditText username = FindViewById<EditText>(Resource.Id.username);
-            EditText password = FindViewById<EditText>(Resource.Id.password);
-            EditText email = FindViewById<EditText>(Resource.Id.email);
-            Button submit = FindViewById<Button>(Resource.Id.submit);
-            TextView result=FindViewById<TextView>(Resource.Id.result);
+            username = FindViewById<EditText>(Resource.Id.username);
+            password = FindViewById<EditText>(Resource.Id.password);
+            email = FindViewById<EditText>(Resource.Id.email);
+            submit = FindViewById<Button>(Resource.Id.submit);
+            result=FindViewById<TextView>(Resource.Id.result);
             submit.Click += delegate{ result.Text = "username :"+username.Text+"\npassword :"+password.Text+"\nemail :"+email.Text; };
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
