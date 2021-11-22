d25da5edfe84cec71773d5e3d2fd50f984d351b2
asuravadha <rr.easwar@gmail.com>
2021-11-22 10:15:54 +0530

    init

---
+++ ImageAdapter.cs
@@ -0,0 +1,50 @@
+﻿using Android.Content;
+using Android.Views;
+using Android.Widget;
+
+public class ImageAdapter : BaseAdapter
+{
+    Context context;
+
+    public ImageAdapter(Context context)
+    {
+        this.context = context;
+    }
+   
+    public override int Count { 
+        get {
+            return images.Length;
+                } 
+    }
+    public override long GetItemId(int position) {
+        return 0;
+    }
+
+    public override Java.Lang.Object GetItem(int position)
+    {
+        return null;
+    }
+
+    public override View GetView(int position, View convertView, ViewGroup parent)
+    {
+        ImageView imageView;
+        if (convertView == null)
+        {
+            imageView=new ImageView(context);
+            imageView.LayoutParameters = new GridView.LayoutParams(100, 100);
+            imageView.SetScaleType(ImageView.ScaleType.CenterCrop);
+        }
+        
+        else
+        {
+            imageView = (ImageView)convertView;
+        }
+        imageView.SetImageResource(images[position]);
+        return imageView;
+}
+
+    int[] images =
+    {
+    Resource.Drawable.b,Resource.Drawable.b,Resource.Drawable.b,Resource.Drawable.b,
+};
+}
--- MainActivity.cs
+++ MainActivity.cs
@@ -17,8 +17,17 @@ namespace suniratech_xamarin
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+            var listView = FindViewById<ListView>(Resource.Id.listView1);
+            var data = new string[] { "Eswar", "hi", "mahanth", "shiv", "karan" };
+            listView.Adapter = new ArrayAdapter(this, Resource.Layout.listview, data);
 
-            
+
+            var gridView = FindViewById<GridView>(Resource.Id.gridview1);
+            gridView.Adapter = new ImageAdapter(this);
+            gridView.ItemClick += delegate (object sender, AdapterView.ItemClickEventArgs args)
+              {
+                  Toast.MakeText(this, args.Position.ToString(), ToastLength.Short).Show();
+              };
            
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
